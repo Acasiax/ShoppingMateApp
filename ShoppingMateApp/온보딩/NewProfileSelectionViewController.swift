9ec9c7bb555aae20d4a45f1ce277bdfdef83777c
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-15 14:07:50 +0900

    프로필 선택 컬렉션 뷰 구성, 셀 파일 생성 UI, 오토레이조정,

--- ShoppingMateApp/온보딩/NewProfileSelectionViewController.swift
+++ ShoppingMateApp/온보딩/NewProfileSelectionViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class NewProfileSelectionViewController: UIViewController {
     
     weak var delegate: ProfileSelectionDelegate?
+    private let profiles: [String] = ["profile_0", "profile_1", "profile_2", "profile_3", "profile_4", "profile_5", "profile_6", "profile_7", "profile_8", "profile_9", "profile_10", "profile_11"]
     
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -24,6 +25,8 @@ class NewProfileSelectionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        collectionView.delegate = self
+        collectionView.dataSource = self
         setupViews()
         setupConstraints()
         
@@ -39,4 +42,28 @@ class NewProfileSelectionViewController: UIViewController {
         }
     }
     
+}
+
+extension NewProfileSelectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return profiles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+           let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProfileCell", for: indexPath) as! ProfileCell
+           let profileName = profiles[indexPath.item]
+           let image = UIImage(named: profileName)
+           image?.accessibilityIdentifier = profileName
+           cell.imageView.image = image
+           
+          
+           
+           return cell
+       }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedProfile = profiles[indexPath.item]
+        delegate?.didSelectProfileImage(named: selectedProfile)
+        self.view.removeFromSuperview()
+        self.removeFromParent()
+    }
 }
