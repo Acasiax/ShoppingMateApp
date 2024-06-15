4a880e1e8d93a5a890636da4eb1416b788acb8f9
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-15 14:23:35 +0900

    랜덤으로 프로필 이미지를 표시하는 UIImageView 생성, 코너레디어스 설정

--- ShoppingMateApp/온보딩/ProfileImageView.swift
+++ ShoppingMateApp/온보딩/ProfileImageView.swift
@@ -8,13 +8,30 @@
 import UIKit
 
 class ProfileImageView: UIView {
-
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        let profileImages = ["profile_0", "profile_1", "profile_2", "profile_3", "profile_4", "profile_5", "profile_6", "profile_7", "profile_8", "profile_9", "profile_10", "profile_11"]
+        let randomImageName = profileImages.randomElement() ?? "profile_0"
+        imageView.image = UIImage(named: randomImageName)
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 50
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 4
+        imageView.layer.borderColor = UIColor.orange.cgColor
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
-          super.init(frame: frame)
-      }
-      
-      required init?(coder: NSCoder) {
-          fatalError("init(coder:) has not been implemented")
-      }
-  
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        addSubview(imageView)
+    }
 }
