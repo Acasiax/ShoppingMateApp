3080ba239ce6369f9801073e7f1141c9685bf4a8
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-15 14:16:24 +0900

    프로필 화면 뷰, 골조작업

---
+++ ShoppingMateApp/온보딩/ProfileImageView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileImageView.swift
+//  ShoppingMateApp
+//
+//  Created by 이윤지 on 6/15/24.
+//
+
+import UIKit
+
+class ProfileImageView: UIView {
+
+    override init(frame: CGRect) {
+          super.init(frame: frame)
+      }
+      
+      required init?(coder: NSCoder) {
+          fatalError("init(coder:) has not been implemented")
+      }
+  
+}
---
+++ ShoppingMateApp/온보딩/SettingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingViewController.swift
+//  ShoppingMateApp
+//
+//  Created by 이윤지 on 6/15/24.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+}
