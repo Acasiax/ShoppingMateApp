301b0a71c80c2bc668770c0b9372881af974c476
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-15 13:46:55 +0900

    이미지 배열 생성, 백그라운드 설정

--- ShoppingMateApp/온보딩/NewProfileSelectionViewController.swift
+++ ShoppingMateApp/온보딩/NewProfileSelectionViewController.swift
@@ -11,11 +11,11 @@ class NewProfileSelectionViewController: UIViewController {
 
     weak var delegate: ProfileSelectionDelegate?
     
-    private let profiles: [String] = ["profile_0", "profile_1", "profile_2", "profile_3", "profile_4", "profile_5", "profile_6", "profile_7", "profile_8", "profile_9", "profile_10", "profile_11",]
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .white
         
     }
     
