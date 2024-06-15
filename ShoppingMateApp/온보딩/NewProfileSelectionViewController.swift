975a55001dadb24773e657e18166619203a445a2
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-15 13:48:39 +0900

    프로필 선택 컬렉션뷰에 들어갈 UI 세팅, 뷰추가, 제약조건 설정

--- ShoppingMateApp/온보딩/NewProfileSelectionViewController.swift
+++ ShoppingMateApp/온보딩/NewProfileSelectionViewController.swift
@@ -8,17 +8,35 @@
 import UIKit
 
 class NewProfileSelectionViewController: UIViewController {
-
+    
     weak var delegate: ProfileSelectionDelegate?
     
-   
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 80, height: 80)
+        layout.minimumInteritemSpacing = 20
+        layout.minimumLineSpacing = 20
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupViews()
+        setupConstraints()
         
     }
     
-
-
+    private func setupViews() {
+        view.addSubview(collectionView)
+    }
+    
+    private func setupConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
 }
