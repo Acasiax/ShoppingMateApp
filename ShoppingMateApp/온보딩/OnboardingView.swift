ace3fde169e153a56a283b8679964efb902f1ca8
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-15 13:16:39 +0900

    온보딩 - 오토레이아웃 제약 조건 추가

--- ShoppingMateApp/온보딩/OnboardingView.swift
+++ ShoppingMateApp/온보딩/OnboardingView.swift
@@ -10,44 +10,62 @@ import SnapKit
 
 class OnboardingView: UIViewController {
     private let titleLabel: UILabel = {
-           let label = UILabel()
-           label.text = "MeaningOut"
-           label.font = UIFont.systemFont(ofSize: 45, weight: .heavy)
-           label.textColor = UIColor.orange
-           label.textAlignment = .center
-           return label
-       }()
+        let label = UILabel()
+        label.text = "MeaningOut"
+        label.font = UIFont.systemFont(ofSize: 45, weight: .heavy)
+        label.textColor = UIColor.orange
+        label.textAlignment = .center
+        return label
+    }()
     
     private let launchImageView: UIImageView = {
-            let imageView = UIImageView()
-            imageView.image = UIImage(named: "launch")
-            imageView.contentMode = .scaleAspectFill
-            return imageView
-        }()
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "launch")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
     
     private let startButton: UIButton = {
-            let button = UIButton(type: .system)
-            button.setTitle("시작하기", for: .normal)
-            button.backgroundColor = .orange
-            button.setTitleColor(.white, for: .normal)
-            button.layer.cornerRadius = 8
-            return button
-        }()
+        let button = UIButton(type: .system)
+        button.setTitle("시작하기", for: .normal)
+        button.backgroundColor = .orange
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 8
+        return button
+    }()
     
     override func viewDidLoad() {
-            super.viewDidLoad()
-            view.backgroundColor = .white
-            setupViews()
-            setupConstraints()
-        }
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupViews()
+        setupConstraints()
+    }
     
     private func setupViews() {
-           view.addSubview(titleLabel)
-           view.addSubview(launchImageView)
-           view.addSubview(startButton)
-       }
+        view.addSubview(titleLabel)
+        view.addSubview(launchImageView)
+        view.addSubview(startButton)
+    }
     private func setupConstraints() {
         
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(40)
+            make.centerX.equalToSuperview()
+        }
+        
+        launchImageView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview().multipliedBy(0.6)
+            make.height.equalTo(launchImageView.snp.width).multipliedBy(1.2)
+        }
+        
+        startButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-40)
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview().multipliedBy(0.8)
+            make.height.equalTo(50)
+        }
         
         
     }
