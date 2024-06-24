1390ae25f5bf694d6294a6d1e194b97e8ea50c43
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-24 20:05:20 +0900

    프로필 오토레이아웃 확장 메서드로 분리, 리팩

--- ShoppingMateApp/온보딩/프로필설정/ProfileSettingViewController.swift
+++ ShoppingMateApp/온보딩/프로필설정/ProfileSettingViewController.swift
@@ -113,54 +113,7 @@ class ProfileSettingViewController: UIViewController {
             contentView.addSubview(passButton)
         }
     }
-    
-    private func setupConstraints() {
-        profileImageView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(40)
-            make.centerX.equalToSuperview()
-            make.width.height.equalTo(100)
-        }
-        
-        contentView.snp.makeConstraints { make in
-            make.top.equalTo(profileImageView.snp.bottom).offset(20)
-            make.left.right.bottom.equalToSuperview()
-        }
-        
-        nicknameTextField.snp.makeConstraints { make in
-            make.top.equalTo(contentView.snp.top).offset(35)
-            make.left.equalToSuperview().offset(40)
-            make.right.equalToSuperview().offset(-40)
-        }
-        
-        bottomLineView.snp.makeConstraints { make in
-            make.top.equalTo(nicknameTextField.snp.bottom).offset(10)
-            make.left.equalToSuperview().offset(40)
-            make.right.equalToSuperview().offset(-40)
-            make.height.equalTo(1)
-        }
-        noteLabel.snp.makeConstraints { make in
-            make.top.equalTo(bottomLineView.snp.bottom).offset(10)
-            make.left.equalToSuperview().offset(45)
-        }
-        
-        if showCompleteButton {
-            completeButton.snp.makeConstraints { make in
-                make.top.equalTo(noteLabel.snp.bottom).offset(30)
-                make.left.equalToSuperview().offset(40)
-                make.right.equalToSuperview().offset(-40)
-                make.height.equalTo(50)
-            }
-        }
-        
-        if showPassButton {
-            passButton.snp.makeConstraints { make in
-                make.top.equalTo(completeButton.snp.bottom).offset(10)
-                make.left.equalToSuperview().offset(40)
-                make.right.equalToSuperview().offset(-40)
-                make.height.equalTo(50)
-            }
-        }
-    }
+
     
     @objc private func profileImageTapped() {
         let newViewController = NewProfileSelectionViewController()
